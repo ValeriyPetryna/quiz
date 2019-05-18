f8c63014557217ca831be27784cf2329c21479b2
ValeriyPetryna <valeriypetrina@gmail.com>
2019-05-18 12:01:48 +0300

    add first working version

--- server.js
+++ server.js
@@ -1,13 +1,34 @@
-const http = require('http')
-const port = 3000
-const requestHandler = (request, response) => {
-    console.log(request.url)
-    response.end('Server is running !')
-}
-const server = http.createServer(requestHandler)
-server.listen(port, (err) => {
-    if (err) {
-        return console.log('something bad happened', err)
-    }
-    console.log(`server is listening on ${port}`)
+const express = require("express")
+const bodyParser = require('body-parser')
+const app = express()
+const urlencodedParser = bodyParser.urlencoded({extended: false})
+const hostname = '127.0.0.1'
+const port = 8080
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true,
+}))
+
+app.use('/quiz', express.static('quiz'))
+app.get('/', function(req,res) {
+    res.sendFile(__dirname + '/firstpage.html')
+})
+app.get('/secondAnswer', function(req,res) {
+    res.sendFile(__dirname + '/secondpage.html')
+})
+    
+app.post("/firstAnswer", urlencodedParser, function (request, response) {
+    if (request.body.answer == 5) {
+        return response.redirect('/secondAnswer')
+    } else response.send("Try again!")
+})
+app.post("/secondAnswer", urlencodedParser, function (request, response) {
+    if (request.body.answer == 6) {
+        return response.send("Great!")
+    } else response.send("Try again!")
+})
+
+const server = app.listen(port, hostname, () => {
+    console.log(new Date() + `  Server running at http://${hostname}:${port}/`)
 })
